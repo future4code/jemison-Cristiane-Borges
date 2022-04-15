587a80816a686098fb2add998e7c635fb90d6c04
Cristiane de Lima Borges <lima-cristianeborges@hotmail.com>
2022-04-14 22:28:19 -0300

    operadores

---
+++ operadores/index.js
@@ -0,0 +1,68 @@
+/* Exercicio 1 
+a. false
+b.false
+c.false
+d. boolean
+
+Exercicio 2
+
+Ele nao converteu o prompt para receber numeros entao ele entendera que é uma string, portanto nao aparecerá o resultado
+
+
+
+/*Exercicio 3
+
+let primeiroNumero = Number (prompt("Digite um numero"));
+let segundoNumero = Number (prompt("Digite um numero"));
+
+const soma = primeiroNumero+segundoNumero;
+
+console.log("O resultado da soma é", soma);
+
+colocando o conversor "Nunmber" na frente do prompt, ele vai entender que o que se insere é o numero, portando o computador
+vai conseguir entender que + é uma soma*/
+
+/*Exercicio de escrita de codigo
+
+Exercicio 1
+
+/*let suaIdade = Number (prompt("Informe a sua idade"));
+let amigoIdade = Number (prompt("Qual idade do seu melhor amigo(a)?"));
+
+suaIdade = true;
+amigoIdade = false;
+
+let resposta = suaIdade && amigoIdade
+
+console.log("Sua idade é maior do que a do seu melhor amigo?", resposta);*/
+
+/*Exercicio 2
+let numeroPar = Number (prompt ("Insira um numero par"));
+
+let resultado = numeroPar % 2;
+
+console.log("O resto da divisão será", resultado);
+
+quando eu coloco um numero par, é retornado o numero 0, quando ímpar, aparece 1*/
+
+/*let primeiroNumero = Number(prompt("Insira o primeiro numero"));
+let segundoNumero = Number(prompt ("Insira o segundo numero"));
+
+let resultado
+
+resultado = primeiroNumero > segundoNumero
+
+console.log("O primeiro numero é maior que segundo?", resultado);
+
+resultado = primeiroNumero === segundoNumero
+
+console.log("O primeiro numero é igual ao segundo?" , resultado);
+
+
+console.log("O primeiro numero é divisivel pelo segundo?" , resultado);
+
+console.log("O segundo numero é divisivel pelo primeiro?")*/
+
+
+
+
