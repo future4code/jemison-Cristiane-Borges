92789c6389a491a17036dce03aa11bf27515054e
Cristiane de Lima Borges <lima-cristianeborges@hotmail.com>
2022-04-16 16:09:12 -0300

    operadores

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,74 @@
+// Exercicio 1
+// resposta: 10 e 5
+
+//Exercicio 2
+//resposta: 20,10 e 10
+
+//Exercicio 3
+//let horasDiarias = prompt ("Quantas horas diarias voce trabalha por dia");
+//let valorDiario = prompt ("Quanto voce recebe por dia?")
+
+//alert ("Voce recebe R$" , valorDiario , "e" , horasDiarias , "por hora" );
+
+// Exercicio  escrita de codigo 1-
+
+/*let umNome;
+let umaIdade;
+
+console.log(typeof umNome);
+console.log(typeof umaIdade);
+
+Foi impresso a palavra undefined porque minha variavel nao recebeu nenhum valor
+
+let umNome = prompt( "Qual o seu nome?");
+
+
+let umaIdade = prompt("Qual a sua idade?");
+
+console.log(typeof umNome);
+console.log(typeof umaIdade);
+
+o meu nome apareceu como String e a minha idade, por mais que seja um numero inserido, apareceu tambem como string
+
+console.log ("Ola" , umNome, "voce tem", umaIdade, "anos");*/
+
+//  Exercicio  escrita de codigo 2-
+
+/*let trabalha=true;
+let comeu=false;
+let bebeu=true;
+
+console.log("Voce trabalhou hoje?", trabalha);
+console.log("Voce comeu hoje?", comeu);
+console.log("Voce bebeu hoje?", bebeu);*/
+
+// Exercicio  escrita de codigo 3
+
+/*let a=10;
+let b=25;
+
+let c=a;
+
+
+console.log("o valor de a é", a);
+console.log("O valor de b é", b);
+console.log("o valor de c é", c);
+
+a=b;
+b=c;
+
+console.log("Os novos valores sao", a, b);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
